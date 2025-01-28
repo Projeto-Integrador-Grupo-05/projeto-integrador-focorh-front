6970b857de17473b2462710cad7d58bd33d18bf1
ninguemzin <alm1427@hotmail.com>
2025-01-28 14:07:21 -0300

    alterações vini

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 import Navbar from "./components/navbar/NavBar";
 import Footer from "./components/footer/Footer";
-import ListaFuncionarios from "./componentes/funcionarios/listafuncionarios/ListaFuncionarios";
+import ListaFuncionarios from "./components/funcionarios/listafuncionarios/ListaFuncionarios";
 import CardDepartamento from "./components/departamento/CardDepartamento";
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <BrowserRouter>
         <div className="min-h-[80vh]">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/funcionarios" element={<ListaFuncionarios />} />
-            <Route path="/departamentos" element={<CardDepartamento />} />
-          </Routes>
-          <Footer />
+          <div className="min-h-screen flex flex-col">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/funcionarios" element={<ListaFuncionarios />} />
+              <Route path="/departamentos" element={<CardDepartamento />} />
+            </Routes>
+            <Footer />
+          </div>
         </div>
       </BrowserRouter>
     </>
