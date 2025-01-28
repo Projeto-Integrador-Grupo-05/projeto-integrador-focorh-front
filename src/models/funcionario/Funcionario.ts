51df848c3a68467985d6eb15a653aec37d12ee1c
Andre Lins <andrelins2403@hotmail.com>
2025-01-28 12:45:33 -0300

    Criacao do modelo funcionario

---
+++ src/models/funcionario/Funcionario.ts
@@ -0,0 +1,10 @@
+export default interface Funcionario {
+    nome: string;
+    admissao: string;
+    email: string;
+    departamento: string;
+    cargo: string;
+    funcional: string;
+    telefone: string;
+    status: string;
+  }
