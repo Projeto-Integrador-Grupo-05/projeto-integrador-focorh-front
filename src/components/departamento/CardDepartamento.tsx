ce76473872db17460acac0887618439641dc88d2
ninguemzin <alm1427@hotmail.com>
2025-01-28 12:17:49 -0300

    cardDepartamento

---
+++ src/components/departamento/CardDepartamento.tsx
@@ -0,0 +1,200 @@
+function ListaCategorias() {
+  return (
+    <>
+      <div className="p-10 h-screen bg-gray-100">
+        <div className="pb-4 container flex justify-between items-center text-lg">
+          <h1 className="text-xl font-bold text-gray-950 mb-2">Departamento</h1>
+          <button className="tect-sm text-white bg-blue-600 hover:bg-emerald-500  px-3 py-2 rounded-md">
+            Add Departamento
+          </button>
+        </div>
+        <div className="overflow-auto rounded-lg shadow ">
+          <table className="w-full p-12">
+            <thead className="bg-gray-50 border-b-2 border-gray-200">
+              <tr>
+                <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                  Nome
+                </th>
+                <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                  Detalhes
+                </th>
+                <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                  Ações
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-100  ">
+              <tr className="bg-gray-50">
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <a
+                    href="#"
+                    className="font-bold text-blue-500 hover:underline"
+                  >
+                    Desenvolvimento de Software
+                  </a>
+                </td>
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <a className="">
+                    Criação e manutenção de sistemas e aplicativos.
+                  </a>
+                </td>
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <a href="#" className="">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="size-4 text-red-600 hover:underline"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                      />
+                    </svg>
+                  </a>
+                </td>
+              </tr>
+              <tr className="bg-white">
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <a
+                    href="#"
+                    className="font-bold text-blue-500 hover:underline"
+                  >
+                    Suporte Técnico
+                  </a>
+                </td>
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <a className="">
+                    Atendimento e resolução de problemas de TI.{" "}
+                  </a>
+                </td>
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <a href="#" className="">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="size-4 text-red-600 hover:underline"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                      />
+                    </svg>
+                  </a>
+                </td>
+              </tr>
+              <tr className="bg-gray-50">
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <a
+                    href="#"
+                    className="font-bold text-blue-500 hover:underline"
+                  >
+                    Infraestrutura de TI
+                  </a>
+                </td>
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <a className="">
+                    Gerenciamento de servidores, redes e hardware.
+                  </a>
+                </td>
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <a href="#" className="">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="size-4 text-red-600 hover:underline"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                      />
+                    </svg>
+                  </a>
+                </td>
+              </tr>
+              <tr className="bg-white">
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <a
+                    href="#"
+                    className="font-bold text-blue-500 hover:underline"
+                  >
+                    Segurança da Informação
+                  </a>
+                </td>
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <a className="">
+                    Proteção de dados e sistemas contra ameaças.
+                  </a>
+                </td>
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <a href="#" className="">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="size-4 text-red-600 hover:underline"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                      />
+                    </svg>
+                  </a>
+                </td>
+              </tr>
+              <tr className="bg-gray-50">
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <a
+                    href="#"
+                    className="font-bold text-blue-500 hover:underline"
+                  >
+                    Gestão de Dados
+                  </a>
+                </td>
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <a className="">
+                    Organização e análise de informações para o negócio.
+                  </a>
+                </td>
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <a href="#" className="">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="size-4 text-red-600 hover:underline"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                      />
+                    </svg>
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ListaCategorias;
