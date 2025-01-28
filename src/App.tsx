2432d7ff1085996f151352b5c112b3f7eb00156a
ninguemzin <alm1427@hotmail.com>
2025-01-28 13:20:25 -0300

    rota

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/NavBar";
 import Footer from "./components/footer/Footer";
+import ListaFuncionarios from "./componentes/funcionarios/listafuncionarios/ListaFuncionarios";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <div className="min-h-[80vh]">
-          <Navbar/>
+          <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/funcionarios" element={<ListaFuncionarios />} />
           </Routes>
-          <Footer/>
-F        </div>
+          <Footer />F{" "}
+        </div>
       </BrowserRouter>
     </>
   );
